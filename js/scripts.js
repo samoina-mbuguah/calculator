75e1f8eaacae190e8f9634249248a263d815ac50
samoina mbuguah <samoina.mbuguah@student.moringaschool.com>
2021-04-09 11:58:21 +0300

    Add code to determine if a number is even or odd

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
-var add = function(number1, number2) {
+// Below code shows if a number is even or odd
+var myDigit = prompt("Please enter a number to determine if it is even or odd:");
+if (myDigit % 2 === 0) {
+  alert("This is an even number");
+} else {
+  alert("This is an odd number");
+}
+
+var add = function (number1, number2) {
   return number1 + number2;
 };
 
@@ -7,7 +15,7 @@ var number2 = parseInt(prompt("Enter another number:"));
 var result = add(number1, number2);
 alert(result);
 
-var subtract = function(number1, number2) {
+var subtract = function (number1, number2) {
   return number1 - number2;
 };
 
@@ -16,7 +24,7 @@ var number2 = parseInt(prompt("Enter another number:"));
 var result = subtract(number1, number2);
 alert(result);
 
-var multiply = function(number1, number2) {
+var multiply = function (number1, number2) {
   return number1 * number2;
 };
 
@@ -25,7 +33,7 @@ var number2 = parseInt(prompt("Enter another number:"));
 var result = multiply(number1, number2);
 alert(result);
 
-var divide = function(number1, number2) {
+var divide = function (number1, number2) {
   return number1 / number2;
 };
 
